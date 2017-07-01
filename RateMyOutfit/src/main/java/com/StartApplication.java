6813bc545eecbd910624774632859c800bb018e3
SamTsai <uopsdod@gmail.com>
2017-07-02 02:09:29 +0800

    #TASK - 將測試Code調整回SpringTestRunner.java

--- RateMyOutfit/src/main/java/com/StartApplication.java
+++ RateMyOutfit/src/main/java/com/StartApplication.java
@@ -27,7 +27,7 @@ import com.util.Util;
 @ComponentScan({"com"})
 @EnableMBeanExport(defaultDomain="${projectName}")
 @ImportResource("classpath:spring-config.xml")
-public class StartApplication extends SpringBootServletInitializer  implements CommandLineRunner{
+public class StartApplication extends SpringBootServletInitializer{
 	
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
@@ -42,53 +42,6 @@ public class StartApplication extends SpringBootServletInitializer  implements C
 //		Util.getFileLogger().info("Util.getVersion(): ");
 	}
 	
-    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
-    @Autowired
-    DataSource dataSource;
-
-    @Autowired
-    CustomerRepository customerRepository;
-
-    @Transactional(readOnly = true)
-    @Override
-    public void run(String... args) throws Exception {
-
-        System.out.println("DATASOURCE = " + dataSource);
-        
-        System.out.println("\n1.findAll()...");
-        for (Customer customer : customerRepository.findAll()) {
-            System.out.println(customer);
-        }
-
-        System.out.println("\n2.findByEmail(String email)...");
-        for (Customer customer : customerRepository.findByEmail("222@yahoo.com")) {
-            System.out.println(customer);
-        }
-
-        System.out.println("\n3.findByDate(Date date)...");
-        for (Customer customer : customerRepository.findByDate(sdf.parse("2017-02-12"))) {
-            System.out.println(customer);
-        }
-
-        // For Stream, need @Transactional
-        System.out.println("\n4.findByEmailReturnStream(@Param(\"email\") String email)...");
-        try (Stream<Customer> stream = customerRepository.findByEmailReturnStream("333@yahoo.com")) {
-            stream.forEach(x -> System.out.println(x));
-        }
-
-        //System.out.println("....................");
-        //SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        //Date from = sdf.parse("2017-02-15");
-        //Date to = sdf.parse("2017-02-17");
-
-        //for (Customer customer : customerRepository.findByDateBetween(from, to)) {
-        //    System.out.println(customer);
-        //}
-
-        System.out.println("Done!");
-
-//        exit(0);
-    }
 	
 }
